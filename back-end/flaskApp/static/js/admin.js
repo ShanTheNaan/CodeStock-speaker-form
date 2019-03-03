38c2e805799d5f16b8acdfe890088dc9b5746c44
ShanTheNaan <slalani@vols.utk.edu>
2019-03-03 01:10:46 -0500

    more changes

--- back-end/flaskApp/static/js/admin.js
+++ back-end/flaskApp/static/js/admin.js
@@ -12,3 +12,50 @@ function sendCsv() {
     document.getElementById("events").classList.add("active");
     document.getElementById("upload").classList.remove("active");
 }
+
+function displayEvents() {
+    document.getElementById("csv-div").style.display = "none";
+    document.getElementById("events-div").style.display = "block";
+    document.getElementById("events").classList.add("active");
+    document.getElementById("upload").classList.remove("active");
+
+    createCard("na lcs lit", "my house", "any", "123");
+    createCard("lck sux", "korea", "never", "093");
+    createCard("LEC lit", "neverland", "12:12", "10294");
+}
+
+function createCard (title, location, time, uid) {
+    var outterDiv = document.createElement("div");
+    var card = document.createElement("div");
+    var cardContent = document.createElement("div");
+    var cardTitle = document.createElement("span");
+    var loc = document.createElement("p");
+    var date = document.createElement("p");
+    var cardAction = document.createElement("div");
+    var qrlink = document.createElement("a");
+    var fform = document.createElement("a");
+
+    outterDiv.className += "col s6 m4 offset-m1";
+    card.className += "card";
+    cardContent.className += "card-content";
+    cardTitle.className += "card-title";
+    cardTitle.innerText += title;
+    cardAction.className += "card-action";
+    loc.innerText = location;
+    date.innerText = time;
+    qrlink.innerText = "QR-Code";
+    qrlink.href = "qrCode.html?uid="+uid+"&title="+title;
+    fform.innerText = "Feedback Form";
+
+    cardAction.appendChild (qrlink);
+    cardAction.appendChild (fform);
+    cardContent.appendChild (cardTitle);
+    cardContent.appendChild (loc);
+    cardContent.appendChild (date);
+    card.appendChild (cardContent);
+    card.appendChild (cardAction);
+    outterDiv.appendChild (card);
+
+    document.getElementById("events-div").appendChild(outterDiv);
+}
+
