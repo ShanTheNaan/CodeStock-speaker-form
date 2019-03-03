211fcdb33b69ac54af54a9e7987cbdd891f7d6bc
Zac <ztrzil@vols.utk.edu>
2019-03-03 07:03:06 -0500

    fix

--- back-end/flaskApp/mysql_helper.py
+++ back-end/flaskApp/mysql_helper.py
@@ -33,10 +33,7 @@ class MySQLHelper:
     return rows
 
 
-  def get_all_talks_for_user(self, uid):
-    """Use the uid of the user leaving the comment to pull all talks that the
-    user has left comments on.
-    """
+  def get_all_talks_for_speaker(self, uid):
     query = "SELECT bt.TalkId, tt.EventTitle, st.FirstName, st.LastName FROM \
       bridge_table bt INNER JOIN talk_table tt ON tt.TalkId = bt.TalkId INNER JOIN \
       speaker_table st ON st.SpeakerId = bt.SpeakerId WHERE bt.SpeakerId=" + str(uid)
